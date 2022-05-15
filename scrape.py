05696178b90ff51c2f5848ca29c588f798342403
Marcin <mlojek@protonmail.com>
2022-05-15 17:14:37 +0200

    Saving images from each subreddit to its separate subdirectory in images

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import praw
 import requests
+import os
 
 
 def lines_from_file(filepath: str) -> list:
@@ -25,17 +26,19 @@ if __name__ == '__main__':
     reddit = praw.Reddit(client_id=client[0], client_secret=client[1], user_agent=client[2])
 
     # Print top hottest posts' title from every subreddit:
-    # for sub in subreddits:
-    #     hottest_posts = reddit.subreddit(sub).hot(limit=1)
-    #     for post in hottest_posts:
-    #         print(sub)
-    #         print(post.title)
+    for sub in subreddits:
+        os.mkdir(f"images/{sub}")
 
-    for submission in reddit.subreddit("polska_wpz").hot(limit=1):
-        print(submission.url)
-        
-        response = requests.get(submission.url)
+        for submission in reddit.subreddit(sub).hot(limit=10):
+            print(submission.url)
+            
+            response = requests.get(submission.url)
 
-        extension = submission.url.rsplit('.')[-1]
-        with open(f"{submission.id}.{extension}", "wb") as file:
-            file.write(response.content)
+            extension = submission.url.rsplit('.')[-1]
+            try:
+                path = f"images/{sub}/{submission.id}.{extension}"
+                print(path)
+                with open(path, "wb") as file:
+                    file.write(response.content)
+            except:
+                pass
