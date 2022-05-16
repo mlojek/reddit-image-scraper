ac8600419f7f63ac80a53d11cad61ab40e381b11
Marcin <mlojek@protonmail.com>
2022-05-16 11:52:49 +0200

    Implement scrape_user_images

--- scrape.py
+++ scrape.py
@@ -50,6 +50,35 @@ def scrape_subreddit_images(reddit, sub_name: str, post_limit: int) -> None:
     print(f'subreddit {sub_name}\t\tDONE')
 
 
+def scrape_user_images(reddit, user_name: str, post_limit: int) -> None:
+    # Create subdirecotry:
+    if user_name not in os.listdir('images'):
+        os.mkdir(f'images/{user_name}')
+
+    for submission in reddit.redditor(user_name).submissions.hot(limit=post_limit):
+        # Check if the media is in a supported format:
+        extension = submission.url.rsplit('.')[-1]
+        if extension not in EXTENSIONS:
+            continue
+
+        # Prepare file path and name:
+        sub_name = submission.subreddit.display_name
+        file_path = f'images/{user_name}/'
+        file_name = f'{submission.id}_{sub_name}_{user_name}.{extension}'
+
+        # If file already exists, skip
+        if file_name in os.listdir(file_path):
+            continue
+
+        # Download the image and save it:
+        response = requests.get(submission.url)
+        with open(file_path + file_name, "wb") as file:
+            file.write(response.content)
+
+    # Print prompt:
+    print(f'user {user_name}\t\tDONE')
+
+
 if __name__ == '__main__':
     client = lines_from_file('client')
     users = lines_from_file('users')
@@ -60,26 +89,6 @@ if __name__ == '__main__':
                          client_secret=client[1],
                          user_agent=client[2])
 
-    for sub in subreddits:
-        scrape_subreddit_images(reddit, sub, 10)
+    # for sub in subreddits:
+    #     scrape_subreddit_images(reddit, sub, 10)
 
-    # for user in users:
-    #     print("User: ", user, '='*40)
-    #     # Make a subdirectory for the images:
-    #     if user not in os.listdir('images'):
-    #         os.mkdir(f'images/{user}')
-
-    #     for submission in reddit.redditor(user).submissions.hot(limit=1000):
-    #         print(submission.url)
-
-    #         try:
-    #             extension = submission.url.rsplit('.')[-1]
-    #             file_path = f'images/{user}/'
-    #             file_name = f'{submission.id}_{user}.{extension}'
-
-    #             # Save to file:
-    #             response = requests.get(submission.url)
-    #             with open(file_path + file_name, "wb") as file:
-    #                 file.write(response.content)
-    #         except FileNotFoundError:
-    #             pass
